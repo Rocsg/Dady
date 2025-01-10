3d35c7e7480398ddb5d606df0753b137e4576301
Rocsg <romainfernandez06@gmail.com>
2025-01-10 17:57:19 +0100

    -

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Dadypy',
-    version='1.0.0',
+    version='0.1.0',
     description='Dady package',
     author='Romain Fernandez',
     author_email='romain.fernandez@cirad.fr',
